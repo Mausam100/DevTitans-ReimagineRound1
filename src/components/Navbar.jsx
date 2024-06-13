805404877807b75161e37d8adf18681cd4436b9e
Mausam100 <smausam104@gmail.com>
2024-06-13 10:17:39 +0530

    responsive nave ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import Logo from '../assets/icons/logo.png'
 const Navbar = () => {
   return (
     <>
+    {/* offer section */}
     <div className='bg-black text-white'>
       <h2 className=' py-2 pl-4 text-lg sm:text-center'>
         <span className=' font-semibold text-lg tracking-wide'>LUCKY OFFER : </span>
@@ -14,16 +15,36 @@ const Navbar = () => {
           <span className=' font-bold text-xl sm:pl-4 md:pl-4 tracking-wide'>Shop NOW!</span>
           </h2>
     </div>
-     <nav className='flex justify-between px-4 pt-3 items-center'>
-       <div className='flex gap-6'>
+    {/* navbar */}
+     <nav className='flex justify-between px-4 pt-3 items-center md:gap-10 '>
+       <div className='flex gap-6 md:hidden '>
         <IoMenu className='text-2xl' />
         <IoSearch className='text-2xl hidden sm:block md:hidden' />
 
         </div> 
-       <div><img className=' w-32' src={Logo} alt="logo" /></div> 
+        {/* logo */}
+       <div><img className=' w-32' src={Logo} alt="logo" />
+       </div> 
+       {/* hyperlinks */}
+       <div className='hidden md:flex md:gap-32 border-b-2 border-[#272727] text-nowrap  '>
+       <div className=' flex gap-10 text-xl '>
+         <a href="#">Best Sellers</a>
+         <a href="#">Skin+Body Care</a>
+         <a href="#">Hair Care</a>
+       </div>
+       <div className=' flex gap-10 text-xl'>
+        <a href="#">Build Your Routine</a>
+        <a href="#">Track order</a>
+        <a href="#">Blog</a>
+       </div>
+
+       </div>
        <div className='flex gap-6'>
        <LuUser className=' text-2xl' />
-        <CgShoppingCart className='text-2xl hidden sm:block md:hidden' /> </div> 
+        <CgShoppingCart className='text-2xl hidden sm:block ' />
+        <IoSearch className='text-2xl hidden md:block' />
+        
+       </div> 
      </nav>
     </>
   )
