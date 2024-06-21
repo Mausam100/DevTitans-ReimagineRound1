b76919d5ffdf51cef5b82d2ba3f2b94afc45fd5a
omkar-4 <omkar.pro04@gmail.com>
2024-06-21 22:19:10 +0530

    updated can't doubt section 1

--- src/components/Cantdoubt.jsx
+++ src/components/Cantdoubt.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Proofcard from './Proofcard'
-import transparency from '../assets/images/transparency.svg'
 
 function Cantdoubt() {
   return (
@@ -8,24 +7,16 @@ function Cantdoubt() {
         <div className='triple-capcule scroll h-[20%]'></div>
 
         <div className='w-full h-[80%]'>
-            <div className='leftdt flex flex-col bg-green-400 md:flex-row'>
+            <div className='leftdt flex flex-col md:flex-row'>
                 <h1 className='text-3xl uppercase'>CAN’T DOUBT OUR<br />PRODUCTS </h1>
-                <div className='w-full bg-pink-200 p-5'>
-                    <div className='flex flex-col md:flex-row bg-pink-200'>
+                <div className='w-full p-5'>
+                    <div className='flex flex-col md:flex-row'>
                         <Proofcard/>
                         <Proofcard/>
                     </div>
                     <div className='flex flex-col md:flex-row'>
 
-                        <div className='w-full h-24 bg-neutral-300'>
-                            <div className='w-24 h-24 bg-zinc-400 rounded-3xl flex justify-center items-center'>
-                                <img src={transparency} className='w-1/2 h-1/2' alt="" />
-                            </div>
-                            <div className='w-2/3 h-full flex flex-col'>
-                                <h3>Total Transparency</h3>
-                                <p>Full disclosure of ingredients used & their concentration</p>
-                            </div>
-                        </div>
+                        <Proofcard/>
                         <Proofcard/>
                     </div>
                 </div>
--- src/components/Proofcard.jsx
+++ src/components/Proofcard.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import transparency from '../assets/images/transparency.svg'
+
 
 function Proofcard() {
   return (
-    <div>
-        hello
+    <div className='w-full h-24 flex flex-row items-center justify-start gap-5'>
+        <div className='w-24 h-24 bg-zinc-400 rounded-3xl flex justify-center items-center'>
+            <img src={transparency} className='w-1/2 h-1/2' alt="" />
+        </div>
+        <div className='w-2/3 h-full flex flex-col items-start justify-center'>
+            <h3 className='text-2xl'>Total Transparency</h3>
+            <p className='text-md leading-none'>Full disclosure of ingredients used & their concentration</p>
+        </div>
     </div>
   )
 }
