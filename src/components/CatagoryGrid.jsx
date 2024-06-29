820c0c9c24376673ae4d59f4c976b47b0f26c297
Mausam100 <smausam104@gmail.com>
2024-06-29 12:23:34 +0530

    props ready

--- src/components/CatagoryGrid.jsx
+++ src/components/CatagoryGrid.jsx
@@ -1,53 +1,11 @@
 import React from "react";
 import CatagoryCard from "./CatagoryCard";
-
-import babycare from '../assets/images/category/baby_care.jpg';
-import skincare from '../assets/images/category/skin_care.jpg';
-import haircare from '../assets/images/category/hair_care.jpg';
-import bodycare from '../assets/images/category/body_care.jpg';
-import lipcare from '../assets/images/category/lip_care.jpg';
-import eyecare from '../assets/images/category/eye_care.jpg';
-
-const catagory_data = [
-  {
-    num : 1,
-    title : "Baby Care",
-    picsrc : babycare,
-  },
-  {
-    num : 2,
-    title : "Skin Care",
-    picsrc : skincare,
-  },
-  {
-    num : 3,
-    title : "Hair Care",
-    picsrc : haircare,
-  },
-  {
-    num : 4,
-    title : "Body Care",
-    picsrc : bodycare,
-  },
-  {
-    num : 5,
-    title : "Lip Care",
-    picsrc : lipcare,
-  },
-  {
-    num : 6,
-    title : "Eye Care",
-    picsrc : eyecare,
-  },
-]
+import { catagoryData } from "../data/constants.js";
 
 function CatagoryGrid() {
   return (
-    <>
-
     <div className="w-full h-fit py-10 flex flex-col justify-center items-center">
-
-    <div className="sm:w-full lg:w-[20%] h-auto pb-8">
+      <div className="sm:w-full lg:w-[20%] h-auto pb-8">
         <h4 className="text-2xl dg-normal text-center uppercase text-zinc-500 leading-3">
           Shop
         </h4>
@@ -58,21 +16,36 @@ function CatagoryGrid() {
 
       <div className="flex flex-col justify-center items-center gap-10">
         <div className="flex flex-row justify-center items-center gap-10">
-          <CatagoryCard />
-          <CatagoryCard />
+          {catagoryData.slice(0, 2).map((category) => (
+            <CatagoryCard
+              key={category.num}
+              title={category.title}
+              imageSrc={category.picsrc}
+            />
+          ))}
         </div>
         <div className="flex flex-row justify-center items-center gap-10">
-          <CatagoryCard />
-          <CatagoryCard />
+          {catagoryData.slice(2, 4).map((category) => (
+            <CatagoryCard
+              key={category.num}
+              title={category.title}
+              imageSrc={category.picsrc}
+            />
+          ))}
         </div>
         <div className="flex flex-row justify-center items-center gap-10">
-          <CatagoryCard />
-          <CatagoryCard />
+          {catagoryData.slice(4, 6).map((category) => (
+            <CatagoryCard
+              key={category.num}
+              title={category.title}
+              imageSrc={category.picsrc}
+            />
+          ))}
         </div>
       </div>
     </div>
-    </>
   );
 }
 
 export default CatagoryGrid;
+
