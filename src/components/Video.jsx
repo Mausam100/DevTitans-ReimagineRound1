239c54e1131275f3ecf7fd62c60503a1de4afb0f
omkar-4 <omkar.pro04@gmail.com>
2024-07-04 17:27:03 +0530

    updated video size

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,7 +4,7 @@ function Video() {
   return (
     <>
     <div className='flex items-center overflow-hidden justify-center w-full h-auto' >
-        <video className='rounded-xl w-3/4 h-auto aspect-auto object-cover overflow-hidden' src="/3d/render.mp4" autoPlay loop muted></video>
+        <video className='rounded-xl w-3/4 md:w-1/2 h-auto aspect-auto object-cover overflow-hidden' src="/3d/render.mp4" autoPlay loop muted></video>
     </div>
     </>
   )
